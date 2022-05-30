06e4d8474ccd03a180d1bcd26d67c34478353127
sakthisarava <velsakthis5301@gmail.com>
2022-05-30 19:49:41 +0530

    second commit

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-exports.handler = async (event) => {
-    event.Records.forEach((record) => {
+exports.handler = async (event: any) => {
+    event.Records.forEach((record: any) => {
         console.log('Event Name: %s', record.eventName);
         console.log('S3 Request: %j', record.s3)
     });
